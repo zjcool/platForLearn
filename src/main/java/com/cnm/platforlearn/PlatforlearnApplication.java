50c53efc211566d2828737bccb9b56458ead84f0
zj <280828919@qq.com>
2019-02-28 00:14:15 +0800

    filter learn

--- src/main/java/com/cnm/platforlearn/PlatforlearnApplication.java
+++ src/main/java/com/cnm/platforlearn/PlatforlearnApplication.java
@@ -2,8 +2,10 @@ package com.cnm.platforlearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.cnm")
+@ServletComponentScan
 public class PlatforlearnApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/cnm/platforlearn/filter/DemoFilter.java
@@ -0,0 +1,47 @@
+package com.cnm.platforlearn.filter;
+
+
+import org.springframework.core.annotation.Order;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+//越小优先级越高
+@Order(1)
+@WebFilter(filterName = "DemoFilterOne", urlPatterns = "/*", initParams = {@WebInitParam(name = "exclusionUrl", value = "/bbb,/ccc")})
+public class DemoFilter implements Filter {
+
+    private Set<String> exclusionUrl;
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String[] exclusionUrls = filterConfig.getInitParameter("exclusionUrl").split(",");
+        exclusionUrl = new HashSet<>(Arrays.asList(exclusionUrls));
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (!shouldFilter(request)) {
+            chain.doFilter(request, response);
+            return;
+        }
+        System.out.println("i'm filterone!");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    private boolean shouldFilter(ServletRequest request) {
+        return !exclusionUrl.contains(((HttpServletRequest) request).getRequestURI());
+    }
+}
